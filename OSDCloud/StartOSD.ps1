d12097db24648151e34d34e1065a55a438359fc2
obs-hub <github@obscorp.com>
2025-03-31 15:15:31 -0400

    Create StartOSD.ps1
    init

---
+++ OSDCloud/StartOSD.ps1
@@ -0,0 +1,44 @@
+# Set OSDCloudGUI Defaults
+$Global:OSDCloud_Defaults = [ordered]@{
+    BrandName             = "Out of the Box Solutions"
+    BrandColor            = "Blue"
+    OSActivation          = "Retail"
+    OSEdition             = "Pro"
+    OSLanguage            = "en-us"
+    OSImageIndex          = 9
+    OSActivationValues    = @(
+        "Retail"
+    )
+    OSEditionValues       = @(
+        "Pro"
+    )
+    OSLanguageValues      = @(
+        "en-us"
+    )
+    OSVersionValues       = @(
+        "Windows 11"
+    )
+    captureScreenshots    = $false
+    ClearDiskConfirm      = $false
+    restartComputer       = $true
+    updateDiskDrivers     = $false
+    updateFirmware        = $true
+    updateNetworkDrivers  = $false
+    updateSCSIDrivers     = $false
+    WindowsUpdateDrivers  = $false
+    WindowsDefenderUpdate = $false
+    SyncMSUpCatDriverUSB  = $true
+    WindowsUpdate         = $true
+    SetTimeZone           = $true
+}
+
+Write-Output $Global:OSDCloud_Defaults
+
+# Create 'Start-OSDCloudGUI.json' - During WinPE SystemDrive will be 'X:'
+$OSDCloudGUIjson = New-Item -Path "$($env:SystemDrive)\OSDCloud\Automate\Start-OSDCloudGUI.json" -Force
+
+# Covert data to Json and export to the file created above
+$Global:OSDCloud_Defaults | ConvertTo-Json -Depth 10 | Out-File -FilePath $($OSDCloudGUIjson.FullName) -Force
+
+Write-Host -ForegroundColor Green “Starting OSDCloud”
+Start-OSDCloudGUI
