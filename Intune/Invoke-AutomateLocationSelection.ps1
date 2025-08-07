60de3ba77b8a28e4977de6f138f089c8d03143ce
obs-hub <github@obscorp.com>
2025-08-07 16:02:38 -0400

    Update Invoke-AutomateLocationSelection.ps1
    Final script

--- Intune/Invoke-AutomateLocationSelection.ps1
+++ Intune/Invoke-AutomateLocationSelection.ps1
@@ -1,4 +1,4 @@
-Import-Module OBS.PS.Tools.Logging<#
+<#
 .SYNOPSIS
     This script attempts to find a matching location ID for the assigned user.
 .DESCRIPTION
@@ -24,66 +24,60 @@ Function Invoke-AutomateLocationSelection {
 		[String] $ClientSecret,
 		[String] $LogPath = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\AutomateLocationSelection-$(Get-Date -Format yyyyMMdd_HHmmss).log"
 	)
-	
+	Import-Module OBS.PS.Tools.Logging
+
 	# Checkl if location is set
 	If ([int]::TryParse([Environment]::GetEnvironmentVariable('AutomateLocationID', 'Machine'), [ref]0)) {
-		Write-Log -L $LogPath -level error "AutomateLocationID already set to $([Environment]::GetEnvironmentVariable('AutomateLocationID', 'Machine'), exiting"
+		Write-Log -L $LogPath -level error "AutomateLocationID already set to $([Environment]::GetEnvironmentVariable('AutomateLocationID', 'Machine')), exiting"
 		return
 	}
 
-	# Connect to Graph API
-	$SecureSecret = ConvertTo-SecureString $ClientSecret -AsPlainText -Force
-	$MgGraphParams = @{
-		ClientId     = $ClientId
-		TenantId     = $TenantId
-		ClientSecret = $SecureSecret
-	}
-	Connect-MgGraph @MgGraphParams
-	$Context = Get-MgContext
-	if (-not $Context.Account -or -not $Context.TenantId) {
-		Write-Log -L $LogPath -level error "Failed to authenticate with Microsoft Graph"
-		throw "Failed to authenticate with Microsoft Graph"
+	$Body = @{
+		Grant_Type = "client_credentials"
+		Scope = "https://graph.microsoft.com/.default"
+		Client_Id = $ClientID
+		Client_Secret = $ClientSecret
 	}
 
-	# Get current device and assigned user ID from Intune
-	$DeviceName = $env:COMPUTERNAME
-	Write-Log -L $LogPath -level info "Current localhost is $DeviceName"
-	$Device = Get-MgDeviceManagementManagedDevice -Filter "deviceName eq '$DeviceName'" -Top 1
+	$Connection = Invoke-RestMethod `
+		-Uri https://login.microsoftonline.com/$TenantID/oauth2/v2.0/token `
+		-Method POST `
+		-Body $body
 
-	if (-not $Device) {
-		Write-Log -L $LogPath -level error "Device '$DeviceName' not found in Intune"
-		throw "Device '$DeviceName' not found in Intune"
+	$Token = $Connection.access_token
+	
+	$Headers = @{ Authorization = "Bearer $Token" }
+	$Device = Invoke-RestMethod -Headers $Headers -Method GET -Uri "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices?`$filter=deviceName eq 'RED-PF29APS7'" 
+
+	if (-not $Device.value) {
+		Write-Log -L $LogPath -level error "Machine was not found in Intune."
+		throw "Machine was not found in Intune."
+	}
+	$DeviceOwner = $Device.value[0].userId
+	if (-not $DeviceOwner) {		
+		Write-Log -L $LogPath -level error"Machine does not have a primary user assigned."
+		throw "Machine does not have a primary user assigned."
+	}	
+		
+	$DeviceOwnerGroups = Invoke-RestMethod -Headers $Headers -Method GET -Uri "https://graph.microsoft.com/v1.0/users/$DeviceOwner/memberOf"
+	$DeviceOwnerGroup = $DeviceOwnerGroups[0].Value | Where-Object { $_.displayName -like '*Site:*' }
+	
+	if (-not $DeviceOwnerGroup) {
+		Write-Log -L $LogPath -level error "No Intune Site group found for user assigned"
+		throw "No Intune Site group found for user assigned"
 	}
 
-	$UserId = $Device.UserId
-	if (-not $UserId) {
-		Write-Log -L $LogPath -level error "Device '$DeviceName' does not have a primary user assigned"
-		throw "Device '$DeviceName' does not have a primary user assigned"
+	$DeviceOwnerGroupId = $DeviceOwnerGroup.Id
+	Write-Log -L $LogPath -level info "Found group: $($DeviceOwnerGroup.displayName) [$DeviceOwnerGroupId]"
+		
+	$LocationId = $DeviceOwnerGroup.description
+	
+	$Ref = 0
+	if (-not ([int]::TryParse($LocationId, [ref]$Ref) -and $LocationId -gt 0)) {	
+		Write-Log -L $LogPath -level error "LocationId not found or no location ID set"
+		throw "LocationId not found or no location ID set"
 	}
-
-	# Get user group memberships and check for Site group membership
-	$Groups = Get-MgUserMemberOf -UserId $UserId -All
-
-	$Group = $Groups | Where-Object { $_.AdditionalProperties['displayName'] -like '*Site:*' }
-
-	if (-not $Group) {
-		Write-Log -L $LogPath -level error "No Intune Site group found for user assigned to '$DeviceName'"
-		throw "No Intune Site group found for user assigned to '$DeviceName'"
-	}
-
-	$GroupId = $Group.Id
-	Write-Log -L $LogPath -level info "Found group: $($Group.AdditionalProperties['displayName']) [$GroupId]"
-
-
-	# Match LocationId
-	$LocationId = (Get-MgGroup -GroupId $Group.Id).Description
-
-	if (-not $LocationId) {
-		Write-Log -L $LogPath -level error "GroupId '$GroupId' not found or no location ID set"
-		throw "GroupId '$GroupId' not found or no location ID set"
-	}
-
-	$LocationId = $Location.LocationId
+ 
 	Write-Log -L $LogPath -level info "Found LocationId: $LocationId"
 
 	# Set as machine environment variable
